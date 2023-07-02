260dc0ef112600feca3c31154bb0adec25e9bce0
Anas Kharboutli <127590238+Anas-Kharboutli@users.noreply.github.com>
2023-07-02 14:04:03 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Build the app',
+      isCompleted: false,
+    },
+    {
+      text: 'Test it',
+      isCompleted: false,
+    },
+    {
+      text: 'Improve it',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
